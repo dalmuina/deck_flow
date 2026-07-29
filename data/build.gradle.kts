d711e130d527592a44b3883bad1d8e3d5dd99c71
Diego Almuiña <dalmuina@gmail.com>
2026-07-29 11:27:16 +0200

    feat: settings.json new hook

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
+    alias(libs.plugins.ktlint)
 }
 
 extensions.configure<LibraryExtension>  {
